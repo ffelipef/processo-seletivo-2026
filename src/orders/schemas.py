f35fa16b20bf303beea087ecce8aea2cf418b5d3
ffelipef <fefreits1415@gmail.com>
2026-07-16 00:00:02 -0300

    feat: Implementação de cache para o catálogo de produtos

--- src/orders/schemas.py
+++ src/orders/schemas.py
@@ -3,19 +3,21 @@ from uuid import UUID
 from datetime import datetime
 from typing import Optional
 from src.orders.models import OrderStatus
-
+from pydantic import computed_field
 # Dados de cada item dentro do detalhe do pedido
 class OrderItemResponse(BaseModel):
     id: UUID
     product_id: Optional[UUID]
     price_at_purchase: float
     quantity: int
-    
+
+    @computed_field
     @property
     def subtotal(self) -> float:
-        return float(self.price_at_purchase * self.quantity)
+        return round(self.price_at_purchase * self.quantity, 2)
 
-    model_config = ConfigDict(from_attributes=True, json_encoders={property: lambda p: p()})
+    model_config = ConfigDict(from_attributes=True)
+
 
 # Schema de resposta detalhada do Pedido (UC14 / UC15)
 class OrderResponse(BaseModel):
@@ -43,3 +45,16 @@ class PaymentSimulationResponse(BaseModel):
     message: str
     order_id: UUID
     new_status: OrderStatus
+
+# para aceitar o cupom
+class CheckoutRequest(BaseModel):
+    coupon_code: Optional[str] = None
+
+class CouponValidateRequest(BaseModel):
+    coupon_code: str
+
+class CouponValidateResponse(BaseModel):
+    valid: bool
+    subtotal: float
+    discount_amount: float
+    total_price: float
